04557eedfdbcc921427496181627dec21bcf51f3
KisicM <kisic.ra183.2018@uns.ac.rs>
2021-01-11 23:59:46 +0100

    #sortiranje_predmeta
    Reseno!

--- src/view/TablePredmet.java
+++ src/view/TablePredmet.java
@@ -2,7 +2,11 @@ package view;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
@@ -11,6 +15,7 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableRowSorter;
 
 import controller.PredmetController;
+import model.BazaPredmeta;
 import model.Predmet;
 
 public class TablePredmet extends JTable {
@@ -56,7 +61,229 @@ public class TablePredmet extends JTable {
 		sorter.setSortable(3, false);
 		sorter.setSortable(4, false);
 		
-		  
+		this.getTableHeader().addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int col = TablePredmet.getInstance().columnAtPoint(e.getPoint());
+
+				switch (col) {
+				case 0:
+					prva++;
+					druga = 0;
+					treca = 0;
+					cetvrta = 0;
+					peta = 0;
+
+					if ((prva % 2) == 0) {
+
+						// REFERENCE: https://www.codebyamir.com/blog/sort-list-of-objects-by-field-java
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return arg1.getSifra().compareTo(arg0.getSifra());
+							}
+						});
+						
+					//REFERENCE: https://stackoverflow.com/questions/1496143/dynamically-changing-the-column-header-text-in-jtable
+						getColumnModel().getColumn(0).setHeaderValue("Šifra " + "\u25BC");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					} else {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return arg0.getSifra().compareTo(arg1.getSifra());
+							}
+						});
+
+						getColumnModel().getColumn(0).setHeaderValue("Šifra " + "\u25B2");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					}
+					break;
+
+				case 1:
+					prva = 0;
+					druga++;
+					treca = 0;
+					cetvrta = 0;
+					peta = 0;
+
+					if ((druga % 2) == 0) {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return arg1.getNaziv().compareTo(arg0.getNaziv());
+							}
+						});
+
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv " + "\u25BC");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+						
+					} else {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return arg0.getNaziv().compareTo(arg1.getNaziv());
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv " + "\u25B2");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+					}
+					break;
+
+				case 2:
+					prva = 0;
+					druga = 0;
+					treca++;
+					cetvrta = 0;
+					peta = 0;
+
+					if ((treca % 2) == 0) {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return String.valueOf(arg0.getESPB()).compareTo(String.valueOf(arg1.getESPB()));
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB " + "\u25BC");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					} else {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return String.valueOf(arg1.getESPB()).compareTo(String.valueOf(arg0.getESPB()));
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB " + "\u25B2");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					}
+					break;
+
+				case 3:
+					prva = 0;
+					druga = 0;
+					treca = 0;
+					cetvrta++;
+					peta = 0;
+
+					if ((cetvrta % 2) == 0) {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return String.valueOf(arg0.getGodinaStudija()).compareTo(String.valueOf(arg1.getGodinaStudija()));
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija " + "\u25BC");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					} else {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1) {
+								return String.valueOf(arg1.getGodinaStudija()).compareTo(String.valueOf(arg0.getGodinaStudija()));
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija " + "\u25B2");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar");
+						
+					}
+					break;
+
+				case 4:
+					prva = 0;
+					druga = 0;
+					treca = 0;
+					cetvrta = 0;
+					peta++;
+
+					if ((peta % 2) == 0) {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1)  {
+								return String.valueOf(arg1.getSemestar())
+										.compareTo(String.valueOf(arg0.getSemestar()));
+							}
+						});
+						
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar " + "\u25BC");
+							
+					} else {
+						Collections.sort(BazaPredmeta.getInstance().getPredmeti(), new Comparator<Predmet>() {
+
+							@Override
+							public int compare(Predmet arg0, Predmet arg1)  {
+								return String.valueOf(arg0.getSemestar())
+										.compareTo(String.valueOf(arg1.getSemestar()));
+							}
+						});
+						
+						getColumnModel().getColumn(0).setHeaderValue("Šifra");
+						getColumnModel().getColumn(1).setHeaderValue("Naziv");
+						getColumnModel().getColumn(2).setHeaderValue("ESPB");
+						getColumnModel().getColumn(3).setHeaderValue("Godina studija");
+						getColumnModel().getColumn(4).setHeaderValue("Semestar " + "\u25B2");
+						
+					}
+					break;
+
+				default:
+					break;
+
+				}
+
+			}
+		});  
+		
+		
+		
+		
 		
 		for (Predmet p : PredmetController.getInstance().getPredmeti()) {
 			  PredmetController.getInstance().popuniListuPolozili(p); 
