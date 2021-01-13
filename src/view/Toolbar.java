ce2866736ef957537b37cf2ce70344d83a155722
KisicM <kisic.ra183.2018@uns.ac.rs>
2021-01-13 21:45:43 +0100

    #pretraga_predmeta
    :)

--- src/view/Toolbar.java
+++ src/view/Toolbar.java
@@ -37,7 +37,6 @@ public class Toolbar extends JToolBar {
 		
 		super(SwingConstants.HORIZONTAL);
 		
-		
 		JButton btnNew = new JButton();
 		btnNew.setToolTipText("Kreiranje entiteta");
 		btnNew.setIcon(new ImageIcon("images"+ File.separator +"File_new.png"));
